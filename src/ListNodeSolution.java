26bfd26bf37ffc6cbba7ba545687d6520bc6b59b
Caio Lucena <caiolucena.dev@gmail.com>
2025-04-26 17:47:43 +0100

    Added ListNodeSolution, LongestCommonPrefix, RemoveDuplicates and ValidParentheses

---
+++ src/ListNodeSolution.java
@@ -0,0 +1,11 @@
+class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(mergeTwoLists(new ListN));
+
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+    }
+}
---
+++ src/ValidParentheses.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class LongestCommonPrefix {
+
+    public static void main(String[] args) {
+        System.out.println(longestCommonPrefix(new String[]{"dog", "dracecar", "dcar"}));
+
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        String prefix = "";
+        String prefixToCompare = "";
+        Arrays.sort(strs);
+        String smallest = strs[0];
+        String biggest = strs[strs.length - 1];
+
+        for (int i = 0; i < smallest.length(); i++) {
+            prefixToCompare = prefixToCompare + smallest.charAt(i);
+            if (smallest.startsWith(prefixToCompare) && biggest.startsWith(prefixToCompare)) {
+                prefix = prefixToCompare;
+            }
+
+        }
+        return prefix;
+    }
+}
